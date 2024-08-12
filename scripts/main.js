f8a4c585c85508f718c482bf4506c6d71b43292d
karimEd001 <anonKM1999@mail.ru>
2024-08-12 20:12:48 +0300

    add sha256 function

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,22 @@ async function connectMetamask() {
 
 // Привязываем функцию к кнопке
 document.getElementById('connectButton').addEventListener('click', connectMetamask);
+
+async function generateSHA256Hash(message) {
+    const msgBuffer = new TextEncoder().encode(message);   // Преобразуем сообщение в массив байтов
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);  // Вычисляем хэш
+    const hashArray = Array.from(new Uint8Array(hashBuffer));  // Преобразуем буфер в массив байтов
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');  // Преобразуем байты в шестнадцатеричную строку
+    return hashHex;
+}
+
+document.getElementById('generateHashButton').addEventListener('click', async () => {
+    const textInput = document.getElementById('shaText').value;
+    if(textInput.split('').length > 0){
+        const hash = await generateSHA256Hash(textInput);
+        document.getElementById('output').innerText = `sha256 Hash: ${hash}`;
+    }
+    else alert('Teaxtaera empty')
+    
+});
+
